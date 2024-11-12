9dbb97f10a80617ae6b178e471326efc460fecfb
qkrdmsql <kelly030813@kaist.ac.kr>
2024-11-12 19:42:11 +0900

    edit: 변수 props 처리

--- src/components/GamePlay.tsx
+++ src/components/GamePlay.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import sample from "../assets/image_4_2 1.jpg";
 import checkmark from "../assets/check1.png";
+import { NumberLiteralType } from "typescript";
 
 type Position = {
   x: number;
@@ -8,197 +9,55 @@ type Position = {
   pIndex: number;
 }
 
-function GamePlay() {
-  const panel =
-  {
-    polygon: [
-      {
-        all_points_x: [
-          141,
-          141,
-          210,
-          210
-        ],
-        all_points_y: [
-          202,
-          216,
-          216,
-          202
-        ],
-      },
-      {
-        all_points_x: [
-          141,
-          141,
-          210,
-          210
-        ],
-        all_points_y: [
-          228,
-          241,
-          241,
-          228
-        ],
-      },
-      {
-        all_points_x: [
-          160,
-          160,
-          210,
-          210
-        ],
-        all_points_y: [
-          255,
-          268,
-          268,
-          255
-        ],
-      },
-      {
-        all_points_x: [
-          160,
-          160,
-          210,
-          210
-        ],
-        all_points_y: [
-          279,
-          292,
-          292,
-          279
-        ],
-      },
-      {
-        all_points_x: [
-          160,
-          160,
-          210,
-          210
-        ],
-        all_points_y: [
-          305,
-          318,
-          318,
-          305
-        ],
-      },
-      {
-        all_points_x: [
-          160,
-          160,
-          210,
-          210
-        ],
-        all_points_y: [
-          330,
-          343,
-          343,
-          330
-        ],
-      },
-      {
-        all_points_x: [
-          230,
-          230,
-          250,
-          250
-        ],
-        all_points_y: [
-          101,
-          147,
-          147,
-          101
-        ],
-      },
-      {
-        all_points_x: [
-          303,
-          303,
-          353,
-          353
-        ],
-        all_points_y: [
-          175,
-          188,
-          188,
-          175
-        ],
-      },
-      {
-        all_points_x: [
-          303,
-          303,
-          353,
-          353
-        ],
-        all_points_y: [
-          200,
-          213,
-          213,
-          200
-        ],
-      },
-      {
-        all_points_x: [
-          303,
-          303,
-          353,
-          353
-        ],
-        all_points_y: [
-          225,
-          238,
-          238,
-          225
-        ],
-      },
-    ]
-  }
-  const [message, setMessage] = useState('');
-  const [round, setRound] = useState(1);
-  const [score, setScore] = useState(0);
+type Props = {
+  panelsInImage: any;
+  round: number;
+  score: number;
+  check: any[];
+  setCheck: React.Dispatch<React.SetStateAction<any[]>>;
+};
+
+function GamePlay(props: Props) {
   const [count, setCount] = useState(0);
-  const [check, setCheck] = useState(Array(panel.polygon.length).fill(0));
   const [marks, setMarks] = useState<Position[]>([]);
+
   const handleImageClick = (event: React.MouseEvent<SVGElement, MouseEvent>) => {
     const { clientX, clientY } = event;
-    if (count < panel.polygon.length) {
+    if (count < props.panelsInImage.polygon.length) {
       setMarks([...marks, { x: clientX, y: clientY, pIndex: -1}]);
       setCount(count => count + 1);
     }
-    console.log(check);
   };
   const handlePolygonClick = (key: number, event: React.MouseEvent<SVGPolygonElement, MouseEvent>) => {
     const { clientX, clientY } = event;
     event.stopPropagation();
-    if (count < panel.polygon.length) {
+    if (count < props.panelsInImage.polygon.length) {
       setMarks([...marks, { x: clientX, y: clientY, pIndex: key }]);
       setCount(count => count + 1);
-      setCheck(check => check.map((cnt, i) => i === key ? cnt + 1 : cnt))
+      props.setCheck(check => check.map((cnt, i) => i === key ? cnt + 1 : cnt))
     }
   };
   const handleMarkClick = (key: number, event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
     event.stopPropagation();
     setMarks(marks => marks.filter((_, i) => i !== key));
-    const tmp = [...check];
+    const tmp = [...props.check];
     if (marks[key].pIndex != -1) {
-      setCheck(check => check.map((cnt, i) => i === marks[key].pIndex ? cnt - 1 : cnt));
+      props.setCheck(check => check.map((cnt, i) => i === marks[key].pIndex ? cnt - 1 : cnt));
     }
     setCount(count => count - 1);
   }
   return (
     <div className='sm:p-4 md:p-6 lg:p-8'>
       <div className='flex flex-row'>
-        <p>Round {round}</p>
-        <p>{score}</p>
+        <p>Round {props.round}</p>
+        <p>{props.score}</p>
       </div>
-      <p>SolarSee AI는 패널 {panel.polygon.length}개를 찾았어요.</p>
-      <p>최대 {panel.polygon.length}개의 패널을 선택해 주세요.</p>
-      <h2>{message}</h2>
-      <div className='relative flex bg-black w-3/5 aspect-square'>
+      <p>SolarSee AI는 패널 {props.panelsInImage.polygon.length}개를 찾았어요.</p>
+      <p>최대 {props.panelsInImage.polygon.length}개의 패널을 선택해 주세요.</p>
+      <div className='relative flex bg-black aspect-square'>
         <img src={sample} className='w-full select-none aspect-square' alt='' />
         <svg className='absolute left-0 top-0 z-10 w-full h-full' viewBox='0 0 100 100' onClick={handleImageClick}>
-          {panel.polygon.map((pan: any, index: number) => (
+          {props.panelsInImage.polygon.map((pan: any, index: number) => (
             <polygon
               points={pan.all_points_x.map((point: number, i: number) =>
                 `${point * 100 / 393},${pan.all_points_y[i] * 100 / 393}`
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,29 +1,33 @@
 import { useEffect, useState } from "react";
-import sample from "../assets/image_4_2 1.jpg";
 import GameButton from "../components/GameButton";
 import { MarkerType } from "../types/interface";
 import GamePlay from "../components/GamePlay";
 
 function GamePage() {
-  const [message, setMessage] = useState('');
-    const [round, setRound] = useState(1);
-    const [score, setScore] = useState(0);
-    const [check, setCheck] = useState([]);
-    const [panel, setPanel] = useState<MarkerType[]>([]);
+  const [round, setRound] = useState(1);
+  const [score, setScore] = useState(0);
+  const [check, setCheck] = useState<any[]>([]);
+  const [panel, setPanel] = useState<MarkerType[]>([]);
 
-    useEffect(() => {
-      fetch("http://localhost:3000/data/panel.json", {
-        method: "GET",
-      })
-        .then((res) => res.json())
-        .then((data) => setPanel(data.polygon));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3000/data/panel.json", {
+      method: "GET",
+    })
+    .then((res) => res.json())
+    .then((data) => setPanel(data.polygon));
+  }, []);
     
   return (
     <div className='static'>
       <p>태양광 패널 찾기</p>
       <GameButton />
-      <GamePlay />
+      <GamePlay 
+        panelsInImage={panel}
+        round={round}
+        score={score}
+        check={check}
+        setCheck={setCheck}
+      />
     </div>
   );
 }
