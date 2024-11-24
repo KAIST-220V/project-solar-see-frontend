adc60140ede48bedf2b03f5d4b960380f0b8dbf9
skykhs3 <skykhs3@naver.com>
2024-11-25 01:22:24 +0900

    feat: GameStory에 페이지 오른쪽으로 슬라이드 유도하는 텍스트 추가

--- src/components/GameStory.tsx
+++ src/components/GameStory.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Cookies from "js-cookie";
 import { ReactComponent as Logo } from "../assets/logo_100px.svg";
 import img1 from "../assets/page1.jpg";
@@ -11,28 +11,43 @@ type Props = {
 };
 
 function GameStory(props: Props) {
-  const [currentPage, setCurrentPage] = useState(0);
-
   return (
-    <div className="w-screen h-screen font-roboto">
-      <div className="flex justify-between items-center w-full h-[10vh] p-5">
+    <div className="w-screen h-screen font-roboto flex flex-col">
+      <div className="flex justify-between items-center w-full h-[10dvh] p-5">
         <Logo className="w-[7vh] h-[7vh] max-h-xs max-w-xs"></Logo>
-        <p className="text-gray-500"
-          onClick={() => {
-            props.setIsFirstVisit(false);
-            Cookies.set("visited", "true", { expires: 7 });
-          }}>Skip</p>
+        <div className="items-end flex flex-col">
+          <p
+            className="text-gray-500 font-handwriting"
+            onClick={() => {
+              props.setIsFirstVisit(false);
+              Cookies.set("visited", "true", { expires: 7 });
+            }}
+          >
+            Skip
+          </p>
+        </div>
       </div>
-      <div className="flex overflow-auto snap-x snap-mandatory w-full h-[90vh]">
+      <p className="animate-slide font-handwriting text-lg text-end -mb-3 text-gray-500">
+        오른쪽으로 넘겨주세요 {">>"}
+      </p>
+      <div className="flex overflow-auto snap-x snap-mandatory w-full h-[90dvh]">
         <div className="flex-none w-full h-full items-center justify-center snap-start">
           <div className="relative top-[5vh] bg-gray-200 sm: h-[100vw] md:h-[55vh] md: aspect-none">
             <img src={img1} className="w-full h-full object-cover"></img>
           </div>
           <div className="relative top-[5vh] pt-8 pl-8 pr-8 w-full text-center">
-            <p className="text-xl text-black font-extrabold">전력 수요 예측을 어렵게 하는 범인,</p>
-            <p className="text-xl text-black font-extrabold">바로 비계량 태양광이에요</p>
-            <p className="text-sm text-gray-500 mt-5">비계량 태양광은 발전량이 측정되지 않는</p>
-            <p className="text-sm text-gray-500">태양광으로, 피크 전력 수요의 11%나 차지해요</p>
+            <p className="text-xl text-black font-extrabold">
+              전력 수요 예측을 어렵게 하는 범인,
+            </p>
+            <p className="text-xl text-black font-extrabold">
+              바로 비계량 태양광이에요
+            </p>
+            <p className="text-sm text-gray-500 mt-5">
+              비계량 태양광은 발전량이 측정되지 않는
+            </p>
+            <p className="text-sm text-gray-500">
+              태양광으로, 피크 전력 수요의 11%나 차지해요
+            </p>
           </div>
         </div>
         <div className="flex-none w-full h-full items-center justify-center snap-start">
@@ -40,11 +55,19 @@ function GameStory(props: Props) {
             <img src={img2} className="w-full h-full object-cover"></img>
           </div>
           <div className="relative top-[5vh] pt-8 pl-8 pr-8 w-full text-center">
-            <p className="text-xl text-black font-extrabold">그래서 Team 220V는</p>
-            <p className="text-xl text-black font-extrabold">SolarSee 프로젝트를 시작했어요</p>
+            <p className="text-xl text-black font-extrabold">
+              그래서 Team 220V는
+            </p>
+            <p className="text-xl text-black font-extrabold">
+              SolarSee 프로젝트를 시작했어요
+            </p>
             <p className="text-sm text-gray-500 mt-3">SolarSee 프로젝트는</p>
-            <p className="text-sm text-gray-500">항공사진과 최신 AI기술을 활용하여</p>
-            <p className="text-sm text-gray-500">비계량 태양광 패널을 탐색하는 프로젝트이에요</p>
+            <p className="text-sm text-gray-500">
+              항공사진과 최신 AI기술을 활용하여
+            </p>
+            <p className="text-sm text-gray-500">
+              비계량 태양광 패널을 탐색하는 프로젝트이에요
+            </p>
           </div>
         </div>
         <div className="flex-none w-full h-full items-center justify-center snap-start">
@@ -52,10 +75,18 @@ function GameStory(props: Props) {
             <img src={img3} className="w-full h-full object-cover"></img>
           </div>
           <div className="relative top-[5vh] pt-8 pl-8 pr-8 w-full text-center">
-            <p className="text-xl text-black font-extrabold">여러분도 게임을 플레이하면서</p>
-            <p className="text-xl text-black font-extrabold">프로젝트에 참여할 수 있어요</p>
-            <p className="text-sm text-gray-500 mt-5">게임을 플레이하면서 모인 데이터는</p>
-            <p className="text-sm text-gray-500">AI 정확도 향상에 사용될 거예요</p>
+            <p className="text-xl text-black font-extrabold">
+              여러분도 게임을 플레이하면서
+            </p>
+            <p className="text-xl text-black font-extrabold">
+              프로젝트에 참여할 수 있어요
+            </p>
+            <p className="text-sm text-gray-500 mt-5">
+              게임을 플레이하면서 모인 데이터는
+            </p>
+            <p className="text-sm text-gray-500">
+              AI 정확도 향상에 사용될 거예요
+            </p>
           </div>
         </div>
         <div className="flex-none w-full h-full items-center justify-center snap-start">
@@ -63,13 +94,19 @@ function GameStory(props: Props) {
             <img src={img4} className="w-full h-full object-cover"></img>
           </div>
           <div className="relative top-[5vh] pt-8 pl-8 pr-8 w-full text-center">
-            <p className="text-xl text-black font-extrabold">그럼 지구의 미래를 위해서,</p>
-            <p className="text-xl text-black font-extrabold">게임을 시작해 볼까요?</p>
-            <button className="rounded-lg bg-yellow w-full h-[6.45533991vh] mt-5"
+            <p className="text-xl text-black font-extrabold">
+              그럼 지구의 미래를 위해서,
+            </p>
+            <p className="text-xl text-black font-extrabold">
+              게임을 시작해 볼까요?
+            </p>
+            <button
+              className="rounded-lg bg-yellow w-full h-[6.45533991vh] mt-5"
               onClick={() => {
                 props.setIsFirstVisit(false);
                 Cookies.set("visited", "true", { expires: 7 });
-              }}>
+              }}
+            >
               게임하기
             </button>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        slide: "slide 1.5s ease-in-out infinite",
+      },
+      keyframes: {
+        slide: {
+          "0%, 100%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(-10px)" },
+        },
+      },
       colors: {
-        'blue': "#364F85",
-        'yellow': "#FFCD00",
-        'orange': "#FFA629",
+        blue: "#364F85",
+        yellow: "#FFCD00",
+        orange: "#FFA629",
       },
       boxShadow: {
-        custom: "2px 2px 6px rgba(0, 0, 0, 0.1), -1px -1px 1px rgba(0, 0, 0, 0.05)", // 커스텀 그림자
+        custom:
+          "2px 2px 6px rgba(0, 0, 0, 0.1), -1px -1px 1px rgba(0, 0, 0, 0.05)", // 커스텀 그림자
       },
     },
     fontFamily: {
-      'roboto': ['Roboto', 'sans-serif'],
-      'handwriting': ['Comic Sans MS', 'cursive'],
-      'nanum': ['Nanum Barun Gothic', 'sans-serif']
+      roboto: ["Roboto", "sans-serif"],
+      handwriting: ["Comic Sans MS", "cursive"],
+      nanum: ["Nanum Barun Gothic", "sans-serif"],
     },
   },
   plugins: [],
